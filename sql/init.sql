2dd4222060b6869c94d1fb79afa7a3155fc9d19e
Giovanni Agenor Marchi <giovanni.marchi@compasso.com.br>
2023-05-12 20:20:08 -0300

    new

--- sql/init.sql
+++ sql/init.sql
@@ -1,38 +1,35 @@
 #create databases
-CREATE DATABASE IF NOT EXISTS db_01_cliente_ms;
-CREATE DATABASE IF NOT EXISTS db_02_comercial_ms;
-CREATE DATABASE IF NOT EXISTS db_03_financeiro_ms;
-CREATE DATABASE IF NOT EXISTS db_04_fornecedor_ms;
-CREATE DATABASE IF NOT EXISTS db_05_funcionario_ms;
-CREATE DATABASE IF NOT EXISTS db_06_produto_ms;
-CREATE DATABASE IF NOT EXISTS sonarqube_db;
+CREATE DATABASE IF NOT EXISTS db_01_customer_ms;
+CREATE DATABASE IF NOT EXISTS db_02_sales_ms;
+CREATE DATABASE IF NOT EXISTS db_03_products_ms;
+-- CREATE DATABASE IF NOT EXISTS db_03_financeiro_ms;
+-- CREATE DATABASE IF NOT EXISTS db_04_fornecedor_ms;
+-- CREATE DATABASE IF NOT EXISTS db_05_funcionario_ms;
+
+-- CREATE DATABASE IF NOT EXISTS sonarqube_db;
 
 #create ms users
-CREATE USER 'cliente-user'@'%' IDENTIFIED BY 'password';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_01_cliente_ms`.* TO 'cliente-user'@'%';
+CREATE USER 'customer-user'@'%' IDENTIFIED BY 'password';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_01_customer_ms`.* TO 'customer-user'@'%';
 FLUSH PRIVILEGES;
 
-CREATE USER 'comercial-user'@'%' IDENTIFIED BY 'password';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_02_comercial_ms`.* TO 'comercial-user'@'%';
+CREATE USER 'sales-user'@'%' IDENTIFIED BY 'password';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_02_sales_ms`.* TO 'sales-user'@'%';
 FLUSH PRIVILEGES;
 
-CREATE USER 'financeiro-user'@'%' IDENTIFIED BY 'password';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_03_financeiro_ms`.* TO 'financeiro-user'@'%';
+CREATE USER 'products-user'@'%' IDENTIFIED BY 'password';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_03_products_ms`.* TO 'products-user'@'%';
 FLUSH PRIVILEGES;
 
-CREATE USER 'fornecedor-user'@'%' IDENTIFIED BY 'password';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_04_fornecedor_ms`.* TO 'fornecedor-user'@'%';
-FLUSH PRIVILEGES;
+-- CREATE USER 'financeiro-user'@'%' IDENTIFIED BY 'password';
+-- GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_03_financeiro_ms`.* TO 'financeiro-user'@'%';
+-- FLUSH PRIVILEGES;
 
-CREATE USER 'funcionario-user'@'%' IDENTIFIED BY 'password';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_05_funcionario_ms`.* TO 'funcionario-user'@'%';
-FLUSH PRIVILEGES;
+-- CREATE USER 'fornecedor-user'@'%' IDENTIFIED BY 'password';
+-- GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_04_fornecedor_ms`.* TO 'fornecedor-user'@'%';
+-- FLUSH PRIVILEGES;
 
-CREATE USER 'produto-user'@'%' IDENTIFIED BY 'password';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_06_produto_ms`.* TO 'produto-user'@'%';
-FLUSH PRIVILEGES;
+-- CREATE USER 'funcionario-user'@'%' IDENTIFIED BY 'password';
+-- GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `db_05_funcionario_ms`.* TO 'funcionario-user'@'%';
+-- FLUSH PRIVILEGES;
 
-#create sonarqube
-CREATE USER 'sonarqube'@'%' IDENTIFIED BY 'sonarqube';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `sonarqube_db`.* TO 'sonarqube'@'%';
-FLUSH PRIVILEGES;
